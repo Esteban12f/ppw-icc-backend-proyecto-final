0dc4b6c7d63a1acd9cb6be0a18dafa2f89e36977
christianastudillo <chris.astu134@gmail.com>
2026-07-27 11:26:16 -0500

    feat: agregar paginacion y ordenamiento a los listados de sessions

--- src/main/java/ec/edu/ups/icc/academicevents/sessions/repositories/SessionRepository.java
+++ src/main/java/ec/edu/ups/icc/academicevents/sessions/repositories/SessionRepository.java
@@ -2,39 +2,23 @@ package ec.edu.ups.icc.academicevents.sessions.repositories;
 
 import ec.edu.ups.icc.academicevents.sessions.entities.SessionEntity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 import java.util.Optional;
 
 public interface SessionRepository
         extends JpaRepository<SessionEntity, Long> {
 
-    @EntityGraph(
-            attributePaths = {
-                    "event"
-            }
-    )
-    List<SessionEntity>
-    findAllByEvent_IdOrderByStartAtAsc(Long eventId);
+    @EntityGraph(attributePaths = {"event"})
+    Page<SessionEntity> findAllByEvent_Id(Long eventId, Pageable pageable);
 
-    @EntityGraph(
-            attributePaths = {
-                    "event"
-            }
-    )
-    Optional<SessionEntity>
-    findById(Long id);
+    @EntityGraph(attributePaths = {"event"})
+    Optional<SessionEntity> findById(Long id);
 
-    @EntityGraph(
-            attributePaths = {
-                    "event"
-            }
-    )
-    List<SessionEntity>
-    findAllByStartAtAfterOrderByStartAtAsc(
-            OffsetDateTime moment
-    );
+    @EntityGraph(attributePaths = {"event"})
+    Page<SessionEntity> findAllByStartAtAfter(OffsetDateTime moment, Pageable pageable);
 }
