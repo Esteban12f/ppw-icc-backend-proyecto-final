951d5f116ac815e0c142962f0c7188b254db65ea
Erick Paucar <erickvzh@MacBook-Pro-de-Erick.local>
2026-07-27 20:44:40 -0500

    feat: agregar RegistrationRepository

---
+++ src/main/java/ec/edu/ups/icc/academicevents/registrations/repositories/RegistrationRepository.java
@@ -0,0 +1,34 @@
+package ec.edu.ups.icc.academicevents.registrations.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ec.edu.ups.icc.academicevents.registrations.entities.RegistrationEntity;
+
+public interface RegistrationRepository
+        extends JpaRepository<RegistrationEntity, Long> {
+
+    @EntityGraph(attributePaths = {"event", "participant"})
+    Optional<RegistrationEntity> findById(Long id);
+
+    @EntityGraph(attributePaths = {"event", "participant"})
+    Page<RegistrationEntity> findAllByParticipant_Id(
+            Long participantId,
+            Pageable pageable
+    );
+
+    @EntityGraph(attributePaths = {"event", "participant"})
+    Page<RegistrationEntity> findAllByEvent_Id(
+            Long eventId,
+            Pageable pageable
+    );
+
+    boolean existsByEvent_IdAndParticipant_Id(
+            Long eventId,
+            Long participantId
+    );
+}
